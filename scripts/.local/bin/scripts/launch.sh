cb3931fd747242d880d04a7b737ca46fc4baf3bf
hainguyen <hainguyentien@hotmail.com>
2019-12-14 08:08:11 +0700

    add wallpaper and compton and dunst notification

--- scripts/.local/bin/scripts/launch.sh
+++ scripts/.local/bin/scripts/launch.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env sh
-
-# Terminate already running bar instances
-killall -q polybar
-
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload main &
-  done
-else
-  polybar --reload example &
-fi
+done
